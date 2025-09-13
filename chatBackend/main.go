c5ef5dec90d99746feb2d29de9f5ba30b54f448d
Dev Aggarwal <devaggarwal1009@gmail.com>
2025-09-13 17:52:20 +0530

    chat backend complete

--- chatBackend/main.go
+++ chatBackend/main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+	"sync"
+
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"github.com/gorilla/websocket"
 )
 
 var db *gorm.DB
@@ -52,6 +56,22 @@ func connectDb () error {
 	return nil
 }
 
+func createRoom (c *gin.Context) {
+	var jsonBody struct {
+		Username string `json:"username"`
+	}
+	err := c.BindJSON(&jsonBody);
+	if(err != nil){
+		fmt.Println(jsonBody)
+		c.JSON(500, gin.H{"error": err, "sucess":false})
+		return;
+	}
+	fmt.Println("hi af", jsonBody)
+	var newRoom Room
+	db.Create(&newRoom)
+	c.JSON(http.StatusOK, gin.H{"success": true, "roomCrated": newRoom})
+}
+
 func Signup (c *gin.Context) {
 	var jsonRes struct{
 		Email string `json:"email"`
@@ -77,7 +97,7 @@ func Signup (c *gin.Context) {
 
 	db.Create(&newUser)
 	fmt.Println("successful signup");
-	c.IndentedJSON(500, gin.H{
+	c.IndentedJSON(200, gin.H{
 		"message": "successful signup",
 		"success": true,
 		"user": struct {
@@ -119,19 +139,139 @@ func GetUser (c *gin.Context) {
 	})
 }
 
-var connections []*websocket.Conn
+type userRoom struct {
+	Conn *websocket.Conn
+	Id uint
+	RoomId uint
+}
+
+var activeUsers = struct {
+	sync.RWMutex
+	m map[uint]*userRoom
+}{
+	m: make(map[uint]*userRoom),
+}
+
+var activeRooms = struct {
+	sync.RWMutex
+	m map[uint][]*userRoom
+}{
+	m: make(map[uint][]*userRoom),
+} 
+
+type socketMsg struct {
+	Type string `json:"type"`
+	Username string `json:"username"`
+	Msg string `json:"msg"` 
+	RoomId uint `json:"roomId"`
+}
+
+func joinRoom(userId uint, roomId uint, c *websocket.Conn){
+	activeUsers.RWMutex.Lock();
+	activeRooms.RWMutex.Lock();
+
+	userRoomInfo := activeUsers.m[userId]
+	if(userRoomInfo.RoomId == roomId){
+		fmt.Println("already in room")
+		userRoomInfo.Conn = c;
+		for _, users := range activeRooms.m[roomId] {
+			if(users.Id == userId){
+				users.Conn = c;
+			}
+		}
+	}else{
+		userRoomInfo.RoomId = roomId;	
+		activeRooms.m[roomId] = append(activeRooms.m[roomId], userRoomInfo);
+	}	
+
+	activeUsers.RWMutex.Unlock();
+	activeRooms.RWMutex.Unlock();
+}
+
+func broadcast(sktMsg *socketMsg, roomId uint, c *websocket.Conn, messageType int) {
+	var connection []*websocket.Conn
+	activeRooms.RWMutex.RLock();
+	for _, userRoomInfo := range activeRooms.m[roomId] {
+		if userRoomInfo.Conn != c  {
+			connection = append(connection, userRoomInfo.Conn)
+		}
+	}
+	activeRooms.RWMutex.RUnlock();
+
+	for _, conn := range connection {
+		err := conn.WriteMessage(messageType, []byte(sktMsg.Msg))
+		if(err != nil){
+			fmt.Println("err while broadcasting")
+		}
+	}
+}
 
 func socket(ctx *gin.Context){
 	w := ctx.Writer
 	r := ctx.Request
+
+	username := r.URL.Query().Get("username") 
+	if(username == ""){
+		ctx.JSON(403, gin.H{"message":"no user found", "success":false})
+		return
+	}
+
+	var connUser User
+	dbError := db.Model(&User{}).
+		Select("id", "username").
+		Where("username = ?", username).
+		First(&connUser).Error
+
+	if(dbError != nil){
+		ctx.JSON(403, gin.H{"message":"no user found", "success":false})
+		return;
+	}
+
 
 	c, err := upgrader.Upgrade(w, r, nil);
 	if(err != nil){
 		ctx.JSON(500, gin.H{"message":"failed to upgrade", "success":false})
 		return
 	}
-	connections = append(connections, c);
-	defer c.Close()
+
+	activeUsers.RWMutex.Lock();
+
+	user, ok := activeUsers.m[connUser.Model.ID];
+	if(!ok){
+		activeUsers.m[connUser.Model.ID] = &userRoom{
+			Conn: c,
+			Id: connUser.Model.ID,
+			RoomId: 0,
+		};
+	}else{
+		user.RoomId = 0
+		user.Conn = c
+	}
+
+	activeUsers.RWMutex.Unlock();
+
+	defer func () {
+		var deletedUser userRoom
+		activeUsers.Lock()
+		deletedUser = *activeUsers.m[connUser.Model.ID]
+		delete(activeUsers.m, connUser.Model.ID)
+		activeUsers.Unlock()
+		activeRooms.Lock()
+
+		roomUsers, ok := activeRooms.m[deletedUser.RoomId];
+		if(ok){
+			currUsers := make([]*userRoom, 0, len(roomUsers) - 1)
+			for _, user := range roomUsers {
+				if(user.Id != deletedUser.Id){
+					currUsers = append(currUsers, user)	
+				}
+			}
+
+			activeRooms.m[deletedUser.RoomId] = currUsers;
+		}
+
+		activeRooms.Unlock()
+	}()
 
 	for {
 		mt, message, err := c.ReadMessage();
@@ -141,16 +281,37 @@ func socket(ctx *gin.Context){
 			break;
 		}
 
-		for _,conn := range connections {
-			writeErr := conn.WriteMessage(mt, message);
-			if(writeErr != nil){
-				fmt.Println("message not send: ", err);
-				ctx.JSON(500, gin.H{"message":"message not send", "success":false})
+		var socketMsgJson socketMsg;
+		jsonerr := json.Unmarshal(message, &socketMsgJson)
+		if jsonerr != nil {
+			log.Printf("Error: Failed to unmarshal JSON: %v", jsonerr)
+
+			log.Printf("Raw message that failed to parse: %s", message) 
+			 
+		}
+
+		fmt.Println("socketMsgJson", socketMsgJson);
+
+		if socketMsgJson.Type == "join" {
+			go joinRoom(connUser.Model.ID, socketMsgJson.RoomId, c)
+		}else{
+			dbChat := &Chat{
+				UserId: connUser.Model.ID,
+				Message: socketMsgJson.Msg,
+				RoomId: socketMsgJson.RoomId,
+			}	
+
+			dbRes := db.Create(&dbChat)
+			if dbRes.Error != nil {
+				fmt.Println("error while writing the chat to db");
 				break;
 			}
+			go broadcast(&socketMsgJson, socketMsgJson.RoomId, c, mt)
 		}
 	}
 }
+
+
 
 func main () {
 
@@ -171,6 +332,6 @@ func main () {
 	r.POST("/signup", Signup)
 	r.GET("/user/:username", GetUser)
 	r.GET("/socket", socket)
-
+	r.POST("/createRoom", createRoom)
 	r.Run("localhost:8080")
 }
